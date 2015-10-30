31f9d2dc43f3e14faff8a45384ec2e2fd421ff4b
yoonchangpark <yoonchang.park@gmail.com>
2015-10-30 12:16:34 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
+  config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/intro', {templateUrl: 'partials/intro.html', controller: GenericViewCtrl});
+    $routeProvider.when('/smartdashboard', {templateUrl: 'partials/smartdashboard.html', controller: GenericViewCtrl});
+    $routeProvider.when('/edashboard', {templateUrl: 'partials/edashboard.html', controller: GenericViewCtrl});
+    $routeProvider.when('/pharosjava', {templateUrl: 'partials/pharosjava.html', controller: GenericViewCtrl});
+    $routeProvider.when('/project_b', {templateUrl: 'partials/project_b.html', controller: GenericViewCtrl});
+    $routeProvider.when('/contact', {templateUrl: 'partials/contact.html', controller: ContactViewCtrl});
+    $routeProvider.when('/imprint', {templateUrl: 'partials/imprint.html', controller: GenericViewCtrl});
+    $routeProvider.otherwise({redirectTo: '/intro'});
+  }]);
